99a3e45af30588b4f8421e0865c50c0c7259eda4
RootExE <57043502+msf-Root-ExE@users.noreply.github.com>
2023-07-12 04:11:10 -0400

    projectStart.sh

--- nmap_tcp_scan.sh
+++ nmap_tcp_scan.sh
@@ -17,12 +17,42 @@ cd "${project_name}"
 # Create a file called "nmap"
 mkdir "nmap"
 
-echo "Second file created successfully. The file name is nmap."
+echo "File created successfully. The file name is nmap."
+
+# Create a file called "Evidence"
+mkdir "Evidence"
+
+echo "File created successfully. The file name is Evidence."
+
+# Create a file called "Notes"
+mkdir "Notes"
+
+echo "File created successfully. The file name is Notes."
+
+# Create a file called "Pre-Reqs"
+mkdir "Pre-Reqs"
+
+echo "File created successfully. The file name is Pre-Reqs."
 
 # Change directory to the "nmap" directory
 cd "nmap"
 
 echo "Changed directory to nmap."
+
+# Create a file called "TCP"
+mkdir "TCP"
+
+echo "File created successfully. The file name is TCP."
+
+# Create a file called "UDP"
+mkdir "UDP"
+
+echo "File created successfully. The file name is UDP."
+
+# Change directory to the "TCP" directory
+cd "TCP"
+
+echo "Changed directory to TCP."
 
 # Prompt user for target IP or hostname
 read -p "Enter target IP or hostname: " target
@@ -38,15 +68,49 @@ if [[ -z "$ping_sweep" ]]; then
 fi
 
 echo "Creating txt file with live hosts..."
-echo $ping_sweep > live_hosts_tcp.txt
+echo $ping_sweep > live_hosts.txt
 
 # Perform discovery scan and output in all formats
 echo "Running scan on live hosts...checking for vulns..."
-nmap -sSVC -iL live_hosts_tcp.txt -oA fast_scan_tcp | tee TCPvulnScan.txt
+nmap -sSVC -vv -iL live_hosts.txt -oA fast_scan_tcp | tee TCPvulnScan.txt
 
 
 # Perform full scan including port 0 and output in all formats
 echo "Running full scan including port 0 on full range treating all hosts as live..."
 sudo nmap -sSVC -Pn -p0-65535 $target -oA full_scan_tcp --max-rtt-timeout=950ms --max-retries=9 --version-intensity= 9 --max-scan-delay=50ms --min-rate=10000 --reason --script=banner | tee TCPFullScan.txt
+
+echo "TCP Scans complete."
+
+# Change directory to the "UDP" directory
+
+cd ..
+cd "UDP"
+
+echo "Changed directory to UDP."
+
+echo "UDP Scan Start"
+
+# Run Nmap ping sweep and output live hosts to txt file
+echo "Running Nmap ping sweep on target $target..."
+ping_sweep=$(sudo nmap -sn $target | grep 'Nmap scan report for' | cut -d ' ' -f 5)
+
+# Check if any live hosts found
+if [[ -z "$ping_sweep" ]]; then
+    echo "No live hosts found."
+    exit 0
+fi
+
+echo "Creating txt file with live hosts..."
+echo $ping_sweep > live_hosts.txt
+
+# Perform verbose UDP fast scan and output in all formats
+echo "Running verbose UDP fast scan on live hosts..."
+sudo nmap -sU -vv -Pn --top-ports 100 -iL live_hosts.txt -oA fast_scan_udp | tee UDPfastscan.txt
+
+# Perform UDP full scan output in all formats
+echo "Running full UDP scan on all hosts..."
+sudo nmap -Pn -sUV $target -oA full_scan_upd | tee UDPfullscan.txt
+
+echo "UDP Scans complete."
 
 echo "Scans complete."
