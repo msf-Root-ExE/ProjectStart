c094330926b760c872dac0b48b23d7c1ca0ec483
RootExE <57043502+msf-Root-ExE@users.noreply.github.com>
2023-11-04 17:04:43 +0000

    Update projectStart.sh

--- projectStart.sh
+++ projectStart.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# ASCII Art and Authorship
+echo "__________                  __  ___________        ___________"
+echo "\\______   \\  ____    ____ _/  |_\\_   _____/___  ___\\_   _____/"
+echo " |       _/ /  _ \\  /  _ \\   __\\|    __)_ \\  \\/  / |    __)_ "
+echo " |    |   \\(  <_> )(  <_> )|  |  |        \\ >    <  |        \\"
+echo " |____|_  / \\____/  \\____/ |__| /_______  //__/\\_ \\/_______  /"
+echo "        \\/                              \\/       \\/        \\/"
+echo ""
+echo "# Author: Ross Brereton (https://www.linkedin.com/in/ross-b-673872107/)"
+echo "# Website: https://github.com/msf-Root-ExE"
+echo ""
+
 # Change directory to the desktop
 cd ~/Desktop
 
@@ -17,7 +29,7 @@ cd "${project_name}"
 # Create directories
 mkdir "nmap" "Evidence" "Notes" "Pre-Reqs" "Web-App"
 
-echo "Directories created successfully. The directory names are nmap, Evidence, Notes, Pre-Reqs."
+echo "Directories created successfully. The directory names are nmap, Evidence, Notes, Pre-Reqs, and Web-App."
 
 # Change directory to the "nmap" directory
 cd "nmap"
